e3cadb0bdafeb1d0bb6956b2c5b9ce1db5f088c0
Piotr Szurlej <piteeroo@yahoo.com>
2016-02-14 23:09:13 +0100

    Create Main.java

---
+++ com.EUOM.Launching/Main.java
@@ -0,0 +1,14 @@
+package com.EUOM.Launching;
+
+import com.EUOM.Calculation.GeneralCalculation;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		GeneralCalculation newProgram = new GeneralCalculation();
+		newProgram.runProgram();
+	}
+
+}
